719c52ff3dafcbdaad1351d540146ba3d18deb71
sri10400 <varshinian27@gmail.com>
2025-01-22 21:51:00 +0530

    Fix line endings

---
+++ simple-blog/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const app = express();
+const db = new sqlite3.Database(':memory:');
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Initialize database
+db.serialize(() => {
+    db.run('CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)');
+    db.run('CREATE TABLE posts (id INTEGER PRIMARY KEY, title TEXT, content TEXT, author TEXT)');
+});
+
+// Routes
+app.get('/', (req, res) => {
+    db.all('SELECT * FROM posts', (err, posts) => {
+        res.render('index', { posts });
+    });
+});
+
+app.get('/create', (req, res) => {
+    res.render('create');
+});
+
+app.post('/create', (req, res) => {
+    const { title, content, author } = req.body;
+    db.run('INSERT INTO posts (title, content, author) VALUES (?, ?, ?)', [title, content, author], () => {
+        res.redirect('/');
+    });
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    db.run('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], () => {
+        res.redirect('/');
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:4000');
+});
